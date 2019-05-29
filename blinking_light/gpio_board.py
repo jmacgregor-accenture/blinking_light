4435e323ba24f894f9269fc1948e5fa627f36c8b
John MacGregor <jmacgregor@pillartechnology.com>
2019-05-29 16:23:05 -0400

    gpio board can enable port

--- blinking_light/gpio_board.py
+++ blinking_light/gpio_board.py
@@ -1,5 +1,31 @@
+from blinking_light import __devenv__
+if __devenv__:
+    from blinking_light.my_fake_rpigio import fake_rpigio as GPIO
+else:
+    import RPi.GPIO as GPIO
+
+
 class GpioBoard():
     
-    def __init__(self, portNumber, mode):
+    portEnabled = False
+    portNumber = 0
+    portMode = None
+    boardMode = None
+
+
+    def __init__(self, boardMode):
+        if boardMode == "BCM":
+            boardMode = GPIO.BCM
+
+        GPIO.setwarnings(False)
+        GPIO.setmode(boardMode)
+        self.boardMode = boardMode
+
+    def enablePort(self, portNumber, portMode):
+        if portMode == "OUT":
+            portMode = GPIO.OUT
+        
+        GPIO.setup(portNumber, portMode)
+        self.portEnabled = True
         self.portNumber = portNumber
-        self.mode = mode
+        self.portMode = portMode
--- tests/test_gpioBoard.py
+++ tests/test_gpioBoard.py
@@ -1,9 +1,20 @@
 from blinking_light.gpio_board import GpioBoard
+from blinking_light.my_fake_rpigio import fake_rpigio as GPIO
 
-def test_gpioBoardHasPortAndMode():
+def test_gpioBoardHasBoardMode():
+    mode = "BCM"
+    board = GpioBoard(mode)
+
+    assert board.boardMode == GPIO.BCM
+
+def test_gpioBoardCanEnablePort():
+    boardMode = GPIO.BCM
     port = 18
     mode = "OUT"
-    board = GpioBoard(port, mode)
+    board = GpioBoard(boardMode)
 
+    board.enablePort(port, mode)
+
+    assert board.portEnabled == True
+    assert board.portMode == GPIO.OUT
     assert board.portNumber == port
-    assert board.mode == mode
