70f429402823dc61a604c11cbeca8f36e107c8d5
John MacGregor <jmacgregor@pillartechnology.com>
2019-06-04 13:29:47 -0400

    Circuit setup can add lights

--- app/setup_circuit.py
+++ app/setup_circuit.py
@@ -4,6 +4,11 @@ class CircuitSetup():
 
     def __init__(self, boardConfig):
         self.board = GpioBoard(boardConfig)
+        self.lights = []
 
     def addPort(self, port):
         self.board.addPort(port)
+
+    def addLed(self, led):
+        self.lights.append(led)
+    
--- tests/test_setupCircuit.py
+++ tests/test_setupCircuit.py
@@ -1,21 +1,32 @@
 from app.setup_circuit import CircuitSetup
 from blinking_light.my_fake_rpigio import fake_rpigio as GPIO
 from blinking_light.gpio_port import GpioPort
+from blinking_light.led_light import LedLight
 import pytest
 
 class TestCircuitSetup():
 
     @pytest.fixture
     def circuitSetup(self):
-        setup = CircuitSetup("BCM")
-        return setup
+        return CircuitSetup("BCM")
+    
+    @pytest.fixture
+    def newPort(self):
+        return GpioPort(18, "out")
 
     def test_circuitSetup_has_board(self, circuitSetup):
         
         assert circuitSetup.board.config == GPIO.BCM
 
-    def test_circuitSetup_can_add_ports(self, circuitSetup):
-        port = GpioPort(18, "out")
-        circuitSetup.addPort(port)
+    def test_circuitSetup_can_add_ports(self, circuitSetup, newPort):
+        circuitSetup.addPort(newPort)
 
         assert len(circuitSetup.board.ports) == 1
+
+    def test_circuitSetup_can_add_lights(self, circuitSetup, newPort):
+        circuitSetup.addPort(newPort)
+        led = LedLight(newPort)
+
+        circuitSetup.addLed(led)
+
+        assert len(circuitSetup.lights) == 1
